dc051d81f8ca3ee92f8674ee33f7bd532f9c6909
KOF <ilikekof@126.com>
2017-06-16 17:52:26 +0800

    Implements View.OnClickListener

--- app/src/main/java/com/example/kof/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/kof/uiwidgettest/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.kof.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonChange = (Button) findViewById(R.id.ButtonChange);
+        buttonChange.setOnClickListener(this);
+
+//        Button buttonChange = (Button) findViewById(R.id.ButtonChange);
+//        buttonChange.setOnClickListener(new View.OnClickListener()
+//                                        {
+//                                            @Override
+//                                            public void onClick(View v) {
+//                                                TextView txt_Content = (TextView) findViewById(R.id.txt_Content);
+//                                                txt_Content.setText("kof");
+//                                            }
+//                                        }
+//        );
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.ButtonChange:
+                TextView txt_Content = (TextView) findViewById(R.id.txt_Content);
+                txt_Content.setText("chen");
+//                Toast.makeText(MainActivity.this,"Click!",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
